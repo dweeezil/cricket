8c8ab53877acbe579bf013f2a4192f72939abe6a
Tim Chase <tim@onlight.com>
2012-06-12 11:09:44 -0500

    Add the author's automated patches to the various source files.
    There is apparently a script (probably in the dev-tools) CVS module
    that edits files from the CVS tree and inserts release dates, etc.
    This commit applies those changes as they exist in a 1.0.5 tarball.

--- lib/Common/Version.pm
+++ lib/Common/Version.pm
@@ -1,4 +1,4 @@
-$Common::global::gVersion = "Cricket version !!VERSION!! (!!RELDATE!!)";
+$Common::global::gVersion = "Cricket version 1.0.5 (2004-03-28)";
 
 $Common::global::gVersion =~ s/[!]!VERSION![!]/devel/;
 1;
